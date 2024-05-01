10b8c2485a9d7c856b2f98bfe1d96bbff1a06166
Фугель Владислав <102908868+Fyhel-Vladyslav@users.noreply.github.com>
2024-05-01 11:39:37 +0300

    try

--- App.js
+++ App.js
@@ -1,270 +1,300 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
-import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+// /* eslint-disable prettier/prettier */
+// import React from 'react';
+// import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+// import 'react-native-gesture-handler';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
 
-import Home from './screens/Alarms';
-import Settings from './screens/Edit';
-import Ring from './screens/Ring';
+// import Home from './screens/Alarms';
+// import Settings from './screens/Edit';
+// import Ring from './screens/Ring';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-export default function () {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Alarms"
-          component={Home}
-          options={params => ({
-            ...headerStyles,
-            title: 'Alarms',
-            headerRight: () => (
-              <AddButton
-                title={'+ '}
-                onPress={() => params.navigation.navigate('Edit')}
-              />
-            ),
-          })}
-        />
-        <Stack.Screen
-          name="Edit"
-          component={Settings}
-          options={{...headerStyles, title: 'Alarm'}}
-        />
-        <Stack.Screen
-          name="Ring"
-          component={Ring}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+// export default function () {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Alarms"
+//           component={Home}
+//           options={params => ({
+//             ...headerStyles,
+//             title: 'Alarms',
+//             headerRight: () => (
+//               <AddButton
+//                 title={'+ '}
+//                 onPress={() => params.navigation.navigate('Edit')}
+//               />
+//             ),
+//           })}
+//         />
+//         <Stack.Screen
+//           name="Edit"
+//           component={Settings}
+//           options={{...headerStyles, title: 'Alarm'}}
+//         />
+//         <Stack.Screen
+//           name="Ring"
+//           component={Ring}
+//           options={{headerShown: false}}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 
-function AddButton({title, onPress}) {
-  return (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={onPress}
-      underlayColor="#fff">
-      <Text style={styles.buttonText}>{title}</Text>
-    </TouchableOpacity>
-  );
-}
+// function AddButton({title, onPress}) {
+//   return (
+//     <TouchableOpacity
+//       style={styles.button}
+//       onPress={onPress}
+//       underlayColor="#fff">
+//       <Text style={styles.buttonText}>{title}</Text>
+//     </TouchableOpacity>
+//   );
+// }
 
-export const headerStyles = {
-  headerStyle: {
-    elevation: 0,
-  },
-  headerTintColor: '#000',
-  headerTitleStyle: {
-    fontWeight: 'bold',
-  },
-};
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'transparent',
-    padding: 10,
-  },
-  buttonText: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 25,
-  },
-});
-// import React, { useState, useEffect } from 'react';
-// import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ActivityIndicator } from 'react-native';
-// import { FontAwesome } from '@expo/vector-icons';
-// import FadeInView from './components/FadeInView';
-// import { Audio } from 'expo-av';
-// import * as Permissions from 'expo-permissions';
-// import * as FileSystem from 'expo-file-system';
-// import { InstantSearch } from 'react-instantsearch-native';
-// import algoliasearch from 'algoliasearch/lite';
-
-// import config from './config';
-// import SearchBox from './components/SearchBox';
-// import Hits from './components/Hits';
-
-// const searchClient = algoliasearch(
-//     config.ALGOLIA_APP_ID,
-//     config.ALGOLIA_API_KEY,
-// );
-
-// const recordingOptions = {
-//     // android not currently in use. Not getting results from speech to text with .m4a
-//     // but parameters are required
-//     android: {
-//         extension: '.m4a',
-//         outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
-//         audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
-//         sampleRate: 44100,
-//         numberOfChannels: 2,
-//         bitRate: 128000,
-//     },
-//     ios: {
-//         extension: '.wav',
-//         audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
-//         sampleRate: 44100,
-//         numberOfChannels: 1,
-//         bitRate: 128000,
-//         linearPCMBitDepth: 16,
-//         linearPCMIsBigEndian: false,
-//         linearPCMIsFloat: false,
-//     },
-// };
-
-// const App = () => {
-//     const [recording, setRecording] = useState(null);
-//     const [isFetching, setIsFetching] = useState(false);
-//     const [isRecording, setIsRecording] = useState(false);
-//     const [query, setQuery] = useState('');
-
-//     useEffect(() => {
-//         Permissions.askAsync(Permissions.AUDIO_RECORDING);
-//     }, []);
-
-//     const deleteRecordingFile = async () => {
-//         try {
-//             const info = await FileSystem.getInfoAsync(recording.getURI());
-//             await FileSystem.deleteAsync(info.uri)
-//         } catch(error) {
-//             console.log("There was an error deleting recording file", error);
-//         }
-//     }
-
-//     const getTranscription = async () => {
-//         setIsFetching(true);
-//         try {
-//             const info = await FileSystem.getInfoAsync(recording.getURI());
-//             console.log(`FILE INFO: ${JSON.stringify(info)}`);
-//             const uri = info.uri;
-//             const formData = new FormData();
-//             formData.append('file', {
-//                 uri,
-//                 type: 'audio/x-wav',
-//                 name: 'speech2text'
-//             });
-//             const response = await fetch(config.CLOUD_FUNCTION_URL, {
-//                 method: 'POST',
-//                 body: formData
-//             });
-//             const data = await response.json();
-//             console.log(data);
-//             setQuery(data.transcript);
-//         } catch(error) {
-//             console.log('There was an error reading file', error);
-//             stopRecording();
-//             resetRecording();
-//         }
-//         setIsFetching(false);
-//     }
-
-//     const startRecording = async () => {
-//         const { status } = await Permissions.getAsync(Permissions.AUDIO_RECORDING);
-//         if (status !== 'granted') return;
-
-//         setIsRecording(true);
-//         await Audio.setAudioModeAsync({
-//             allowsRecordingIOS: true,
-//             interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-//             playsInSilentModeIOS: true,
-//             shouldDuckAndroid: true,
-//             interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-//             playThroughEarpieceAndroid: true,
-//         });
-//         const recording = new Audio.Recording();
-
-//         try {
-//             await recording.prepareToRecordAsync(recordingOptions);
-//             await recording.startAsync();
-//         } catch (error) {
-//             console.log(error);
-//             stopRecording();
-//         }
-
-//         setRecording(recording);
-//     }
-
-//     const stopRecording = async () => {
-//         setIsRecording(false);
-//         try {
-//             await recording.stopAndUnloadAsync();
-//         } catch (error) {
-//             // Do nothing -- we are already unloaded.
-//         }
-//     }
-
-//     const resetRecording = () => {
-//         deleteRecordingFile();
-//         setRecording(null);
-//     };
-
-//     const handleOnPressIn = () => {
-//         startRecording();
-//     };
-
-//     const handleOnPressOut = () => {
-//         stopRecording();
-//         getTranscription();
-//     };
-
-//     const handleQueryChange = (query) => {
-//         setQuery(query);
-//     };
-
-//     return (
-//         <SafeAreaView style={{flex: 1}}>
-//             <View style={styles.container}>
-//                 {isRecording &&
-//                     <FadeInView>
-//                         <FontAwesome name="microphone" size={32} color="#48C9B0" />
-//                     </FadeInView>
-//                 }
-//                 {!isRecording &&
-//                     <FontAwesome name="microphone" size={32} color="#48C9B0" />
-//                 }
-//                 <Text>Voice Search</Text>
-//                 <TouchableOpacity
-//                     style={styles.button}
-//                     onPressIn={handleOnPressIn}
-//                     onPressOut={handleOnPressOut}
-//                 >
-//                     {isFetching && <ActivityIndicator color="#ffffff" />}
-//                     {!isFetching && <Text>Hold for Voice Search</Text>}
-//                 </TouchableOpacity>
-//             </View>
-//             <View style={{paddingHorizontal: 20}}>
-//                 <InstantSearch
-//                     indexName={config.ALGOLIA_INDEX}
-//                     searchClient={searchClient}
-//                 >
-//                     <SearchBox query={query} onChange={handleQueryChange} />
-//                     <Hits />
-//                 </InstantSearch>
-//             </View>
-//         </SafeAreaView>
-//     );
+// export const headerStyles = {
+//   headerStyle: {
+//     elevation: 0,
+//   },
+//   headerTintColor: '#000',
+//   headerTitleStyle: {
+//     fontWeight: 'bold',
+//   },
 // };
 
 // const styles = StyleSheet.create({
-//     container: {
-//         marginTop: 40,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//     },
-//     button: {
-//         backgroundColor: '#48C9B0',
-//         paddingVertical: 20,
-//         width: '90%',
-//         alignItems: 'center',
-//         borderRadius: 5,
-//         marginTop: 20,
-//     }
+//   button: {
+//     backgroundColor: 'transparent',
+//     padding: 10,
+//   },
+//   buttonText: {
+//     color: 'black',
+//     fontWeight: 'bold',
+//     fontSize: 25,
+//   },
 // });
+import React, { useState, useRef ,useCallback,useEffect} from 'react';
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
+import AudioRecorderPlayer from 'react-native-audio-record';
+import { Audio } from 'expo-av';
+import MicRecorder from "mic-recorder-to-mp3"
+import Button from "./components/Button";
+import RNFS from 'react-native-fs';
 
-// export default App;
+  const apiKey = "f17d209777f7463d92f4b113328e21cf";
+
+  const headers = {
+    'Authorization': 'f17d209777f7463d92f4b113328e21cf',
+    'Content-Type': 'application/json',
+    'Transfer-Encoding': 'chunked'
+};
+const uri = 'test.wav';
+const App = () => {
+  const recorder = useRef(null) //Recorder
+  const audioPlayer = useRef(null) //Ref for the HTML Audio Tag
+  const [blobURL, setBlobUrl] = useState(null)
+  const [audioFile, setAudioFile] = useState(null)
+  const [isRecording, setIsRecording] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const recording = useRef(null);
+  const sound = useRef(null);
+
+
+  const [uploadURL, setUploadURL] = useState("")
+  const [transcriptID, setTranscriptID] = useState("")
+  const [transcriptData, setTranscriptData] = useState("")
+  const [transcript, setTranscript] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("da");
+      console.log(transcriptData);
+      if (transcriptData.status !== "completed" && isLoading) {
+        checkStatusHandler();
+        console.log("1");
+      } else {
+        console.log("2");
+        setIsLoading(false)
+        setTranscript(transcriptData.text)
+        console.log(transcriptData.text);
+        clearInterval(interval)
+      }
+    }, 10000)
+    return () => clearInterval(interval)
+  },)
+  const handleClick = useCallback(() => {
+    submitTranscriptionHandler();
+}, []); // Dependencies should be added if any variable inside the function is used
+
+  useEffect(() => {
+    //Declares the recorder object and stores it inside of ref
+    recorder.current = new MicRecorder({ bitRate: 128 })
+  }, [])
+
+  const startRecording = async () => {
+    try {
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      recording.current = new Audio.Recording();
+      await recording.current.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.current.startAsync();
+      setIsRecording(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      await recording.current.stopAndUnloadAsync();
+      setIsRecording(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const startPlaying = async () => {
+    try {
+      const { sound: audioSound } = await recording.current.createNewLoadedSoundAsync(
+        { isLooping: false },
+        status => {
+          if (status.didJustFinish) {
+            setIsPlaying(false);
+          }
+        }
+      );
+      sound.current = audioSound;
+      await sound.current.playAsync();
+      setIsPlaying(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const stopPlaying = async () => {
+    try {
+      if (sound.current) {
+        await sound.current.stopAsync();
+      }
+      setIsPlaying(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
+
+  useEffect(() => {
+    if (audioFile) {
+      
+      fetch('https://api.assemblyai.com/v2/upload', {
+        method: 'POST',
+        headers: headers,
+        body: audioFile
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Dasasdas")
+        setUploadURL(data.upload_url);
+        setTranscriptID(data.id);
+    })
+    .catch(error => console.error(error));
+    }
+  }, [audioFile])
+
+  // Submit the Upload URL to AssemblyAI and retrieve the Transcript ID
+  const submitTranscriptionHandler = async () => {
+
+    console.log("sumbm");    
+
+    const wavData = RNFS.readFile(uri, 'base64');
+
+    console.log("wavData");
+    console.log(wavData);
+    fetch('https://api.assemblyai.com/v2/transcript', {
+      method: "POST",
+      headers: {
+        authorization: apiKey,
+        "content-type": "application/json",
+        "transfer-encoding": "chunked",
+      },
+      body: JSON.stringify({
+        audio_data: wavData
+      })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    console.log(data.id);
+    console.log("data.id");
+
+      setTranscriptID(data.id);
+  })
+  .catch(error => console.error(error));
+  }
+
+  // Check the status of the Transcript
+  const checkStatusHandler = async () => {
+    setIsLoading(true)
+    try {
+      await fetch(`https://api.assemblyai.com/v2/transcript/${transcriptID}`, {
+        method: 'GET',
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      console.log("transcriptID")
+      console.log(transcriptID)
+
+        setTranscriptData(data);
+    })
+    .catch(error => console.error(error));
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <View>
+      <Text>React Speech Recognition App</Text>
+      
+      <TouchableOpacity onPress={handleClick}>
+            <Text>Press Me</Text>
+        </TouchableOpacity>
+          
+      {transcriptData.status === "completed" ? (
+        <Text>{transcript}</Text>
+      ) : (
+        <Text>{transcriptData.status}</Text>
+      )}
+      <TouchableOpacity onPress={isRecording ? stopRecording : startRecording}>
+        <Text style={styles.button}>{isRecording ? 'Stop Recording' : 'Start Recording'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={isPlaying ? stopPlaying : startPlaying}>
+        <Text>{isPlaying ? 'Stop Playing' : 'Start Playing'}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default App;
+const styles = StyleSheet.create({
+
+  button: {
+    display: 'flex',
+    margin:100,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
---
+++ components/FadeInView.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Animated, Text, View } from 'react-native';
+
+class FadeInView extends React.Component {
+    state = {
+        fadeAnim: new Animated.Value(0),
+    }
+
+    componentDidMount() {
+        Animated.loop(
+            Animated.timing(
+                this.state.fadeAnim,
+                {
+                    toValue: 1,
+                    duration: 1400,
+                    useNativeDriver: true, // Specify useNativeDriver option
+                },
+            )
+        ).start();
+    }
+
+    render() {
+        let { fadeAnim } = this.state;
+        const { children } = this.props;
+        return (
+            <Animated.View style={{opacity: fadeAnim}}>
+                {children}
+            </Animated.View>
+        )
+    }
+}
+
+export default FadeInView;
